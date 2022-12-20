73ca19f092b37c1cdadfa6e66f6dd7f6b172c3d8
long-doth2202 <long.doth2202@gmail.com>
2022-12-20 13:20:51 +0700

    update backend

--- back-end/main.py
+++ back-end/main.py
@@ -2,10 +2,11 @@ from flask import Flask, request, render_template, send_from_directory, jsonify
 import time
 import json
 import math
+import torch
 import os
 
 from siamese_model import SiameseConvNet, distance_metric
-import torch
+from preprocessing import convert_to_image_tensor, invert_image
 
 app = Flask(__name__, template_folder='templates')
 
@@ -23,17 +24,13 @@ def test_api():
 def index():
     return render_template('mainpage.html')
 
+@app.route('/verify', method=['POST'])
+def verify():
+    pass
+
 def main():
     # For heroku, remove this line. We'll use gunicorn to run the app
     app.run() # app.run(debug=True) 
-
-@app.route("/manifest.json")
-def manifest():
-    return send_from_directory('./frontend/build', 'manifest.json')
-
-@app.route("/favicon.ico")
-def favicon():
-    return send_from_directory('./frontend/build', 'favicon.ico')
 
 if __name__=='__main__':
     main()
